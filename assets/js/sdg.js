8cef6b608ae6d32bd16b4fe70170d93554f577ae
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-06-27 22:28:53 +0000

    Deploying to gh-pages from  @ 367be88d2a10392494088844e2b6888c3bfa2c0a 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2536,8 +2536,6 @@ function getPrecision(precisions, selectedUnit, selectedSeries) {
 function inputData(data) {
   var dropKeys = [];
   
-  dropKeys = ["Observation status","Unit multiplier","Nature of data points","Source details","Frequency of observation","Observation-level footnotes"];
-  
   return convertJsonFormatToRows(data, dropKeys);
 }
 
@@ -2547,14 +2545,6 @@ function inputData(data) {
  */
 function inputEdges(edges) {
   var edgesData = convertJsonFormatToRows(edges);
-  
-  var ignoredDisaggregations = ["Observation status","Unit multiplier","Nature of data points","Source details","Frequency of observation","Observation-level footnotes"];
-  edgesData = edgesData.filter(function(edge) {
-    if (ignoredDisaggregations.includes(edge.To) || ignoredDisaggregations.includes(edge.From)) {
-      return false;
-    }
-    return true;
-  });
   
   return edgesData;
 }
