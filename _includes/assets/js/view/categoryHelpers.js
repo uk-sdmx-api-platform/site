4cb6f41d2b8211a9b97420eff681840963f608e2
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-11 21:43:48 +0100

    Create categoryHelpers.js

---
+++ _includes/assets/js/view/categoryHelpers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Object} args
+ * @return null
+ */
+
+function initialiseCategories(args) {
+
+  var template = _.template($('#categories_template').html());
+  
+	$('#categories').html(template({
+        fields: args.fields
+    }));
+
+}
+                          
