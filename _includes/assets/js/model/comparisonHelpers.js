e2339915216cbe93978e13419392b8f528766d28
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-12 22:47:24 +0100

    Update comparisonHelpers.js

--- _includes/assets/js/model/comparisonHelpers.js
+++ _includes/assets/js/model/comparisonHelpers.js
@@ -9,3 +9,11 @@
 function dataHasReportingType(columns) {
   return columns.includes(REPORTINGTYPE_COLUMN);
 }
+
+/**
+ * @param {Array} reportingTypes
+ * @return {boolean}
+ */
+function dataHasGlobalValues(reportingTypes) {
+  return reportingTypes.includes("Global")
+}
