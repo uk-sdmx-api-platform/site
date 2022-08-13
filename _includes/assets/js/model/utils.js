7dbb3ebd9285111706c25cdcfadd4921ddc51945
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-13 01:13:36 +0100

    Update utils.js

--- _includes/assets/js/model/utils.js
+++ _includes/assets/js/model/utils.js
@@ -29,6 +29,15 @@ function dataHasGeoCodes(columns) {
   return columns.includes(GEOCODE_COLUMN);
 }
 
+
+/**
+ * @param {Array} reportingTypes
+ * @return {boolean}
+ */
+function dataHasGlobalValues(reportingTypes) {
+  return reportingTypes.includes("Global");
+}
+
 /**
  * @param {Array} rows
  * @return {Array} Columns from first row
