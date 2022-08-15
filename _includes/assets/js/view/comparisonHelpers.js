d5bb3895a3fe8bf3b1df99f57ea3aa6e0cd30355
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-15 17:17:09 +0100

    Update comparisonHelpers.js

--- _includes/assets/js/view/comparisonHelpers.js
+++ _includes/assets/js/view/comparisonHelpers.js
@@ -8,7 +8,7 @@ function initialiseFieldsWithGlobalValues(args) {
   var template = _.template($('#categories_template').html());
   
 	$('#categories').html(template({
-	fields: args.fields
+	fields: args.fields,
         fieldsWithGlobalValues: args.fieldsWithGlobalValues
     }));
 
