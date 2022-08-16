2144af4256730fb29cc4e33049441bdca084f04a
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-16 03:15:18 +0100

    Update comparisonHelpers.js

--- _includes/assets/js/model/comparisonHelpers.js
+++ _includes/assets/js/model/comparisonHelpers.js
@@ -10,128 +10,33 @@ function dataHasReportingTypes(columns) {
   return columns.includes(REPORTINGTYPE_COLUMN);
 }
 
-
-
 /**
- * @param {Array} reportingTypes
  * @param {Array} rows
  * @param {Array} columns
- * @return {Array} Field item state by reporting type
+ * @return {Array} Field items and values with global data
  */
-function fieldItemStateReportingType(reportingTypes, rows, columns) {
+function fieldValuesWithGlobalReportingType(rows, columns) {
   var fields = getFieldColumnsFromData(columns);
   return fields.map(function(field) {
-    var values = getUniqueValuesByProperty(field, rows);
+  var values = getUniqueValuesByProperty(field, rows);
     return {
       field: field,
-      hasGlobalValues: fieldHasGlobalValues(field)
-      values: values.map(function(value) {
-        var fieldValuesUsedByReportingType = fieldValuesUsedByReportingType(reportingTypes, rows, columns);
-        return {
-          value: value,
-          hasGlobalValues: fieldValueHasGlobalValues(fieldValuesUsedByReportingType,field,value)
-        };
+      values: fieldValues.filter(function(fieldValue) {
+        return fieldValueHasGlobalValues(field, fieldValue, rows);
       }, this),
     };
   }, this);
 }
 
-/**
- * @param {Array} fieldsWithGlobalValues
- * @param {string} field
- * @return {boolean} 
- */
-function fieldHasGlobalValues(fieldsWithGlobalValues, field) {
-  return fieldsWithGlobalValues.includes(field);
-}
-
-/**
- * @param {Array} fieldsUsedByReportingType
- * @return {Array} Field names
- */
-function fieldsWithGlobalValues(fieldsUsedByReportingType) {
-  return fieldsUsedByReportingType.filter(obj => obj.reportingType == 'Global')[0].fields
-}
-
-
-/**
- * @param {Array} fieldsUsedByReportingType
- * @return {boolean}
- */
-function dataHasGlobalValues(fieldsUsedByReportingType) {
-  return fieldsUsedByReportingType.filter(obj => obj.reportingType == 'Global').length > 0
-}
-
-/**
- * @param {Array} reportingTypes
- * @param {Array} rows
- * @return {Array} Field names by reporting type
- */
-function fieldsUsedByReportingType(reportingTypes, rows, columns) {
-  var fields = getFieldColumnsFromData(columns);
-  return reportingTypes.map(function(reportingType) {
-    return {
-      reportingType: reportingType,
-      fields: fields.filter(function(field) {
-        return fieldIsUsedInDataWithReportingType(field, reportingType, rows);
-      }, this),
-    }
-  }, this);
-}
 
 /**
  * @param {string} field
  * @param {string} reportingType
  * @param {Array} rows
  */
-function fieldIsUsedInDataWithReportingType(field, reportingType, rows) {
+function fieldValueHasGlobalReportingType(field, fieldValue, rows) {
   return rows.some(function(row) {
-    return row[field] && row[REPORTINGTYPE_COLUMN] === reportingType;
+    return row[field] === fieldValue && row[REPORTINGTYPE_COLUMN] === 'Global';
   }, this);
 }
 
-/**
- * @param {Array} fieldValuesWithGlobalValues
- * @param {string} field
- * @return {boolean} 
- */
-function fieldValueHasGlobalValues(fieldValuessWithGlobalValues, field, value) {
-  return fieldValuesUsedByReportingType.filter(obj => obj.field == field)[0]['reportingTypes'].filter(obj => obj.reportingType == 'Global')[0]['values'].includes(value)
-}
-
-/**
- * @param {Array} reportingTypes
- * @param {Array} rows
- * @param {Array} columns
- * @return {Array} Field names by reporting type
- */
-function fieldValuesUsedByReportingType(reportingTypes, rows, columns) {
-  var fields = getFieldColumnsFromData(columns);
-  return fields.map(function(field) {
-    var values = getUniqueValuesByProperty(field, rows);
-    return {
-  		field: field,
-  		reportingTypes: reportingTypes.map(function(reportingType) {
-    		return {
-      		reportingType: reportingType,
-          values: values.filter(function(value) {
-        		return fieldValueIsUsedInDataWithReportingType(field, value, reportingType, rows)
-            ;
-      }, this),
-      	};
-  }, this),
-  }
-  }, this);
-}
-
-
-/**
- * @param {string} field
- * @param {string} reportingType
- * @param {Array} rows
- */
-function fieldValueIsUsedInDataWithReportingType(field, fieldValue, reportingType, rows) {
-  return rows.some(function(row) {
-    return row[field] === fieldValue && row[REPORTINGTYPE_COLUMN] === reportingType;
-  }, this);
-}
