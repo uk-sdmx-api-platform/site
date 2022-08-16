c1d2cdc550852dc12f0cd2c780e350b8d3635efb
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-16 20:53:18 +0100

    Update comparisonHelpers.js

--- _includes/assets/js/view/comparisonHelpers.js
+++ _includes/assets/js/view/comparisonHelpers.js
@@ -10,17 +10,17 @@ function initialiseFieldsWithGlobalValues(args) {
 		return
 	}
 	
-	var template = _.template($('#categories_template').html());
-	$('#categories').html(template({
-		fields: args.fields,
-		fieldValuesWithGlobalReportingType: args.fieldValuesWithGlobalReportingType
-	}));
 	
 	$('.toggle-switch-check').change(function() {
 		if (this.checked) {
 			console.log(this.checked);
 			console.log('fieldsHaveGlobalReportingType: '+args.fieldsHaveGlobalReportingType);
 			$('#toolbar').hide();
+			var template = _.template($('#categories_template').html());
+			$('#categories').html(template({
+				fields: args.fields,
+				fieldValuesWithGlobalReportingType: args.fieldValuesWithGlobalReportingType
+			}));
 			if (args.fieldsHaveGlobalReportingType) {
 				$('#categories').show();
 			}	
