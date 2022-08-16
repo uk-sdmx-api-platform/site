ff60813c05bf7d8fcee58c6370fd8037754c059a
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-16 19:05:53 +0000

    Deploying to gh-pages from  @ 5c0ccad012896632fccb8359b240b082a8f59088 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3283,6 +3283,11 @@ function initialiseFieldsWithGlobalValues(args) {
 		return
 	}
 	
+	$('#categories').html(template({
+		fields: args.fields,
+		fieldValuesWithGlobalReportingType: args.fieldValuesWithGlobalReportingType
+	}));
+	
 	$('.toggle-switch-check').change(function() {
 		if (this.checked) {
 			console.log(this.checked);
@@ -3290,13 +3295,8 @@ function initialiseFieldsWithGlobalValues(args) {
 			$('#toolbar').hide();
 			if (args.fieldsHaveGlobalReportingType) {
 				var template = _.template($('#categories_template').html());
-				$('#categories').html(template({
-				fields: args.fields,
-				fieldValuesWithGlobalReportingType: args.fieldValuesWithGlobalReportingType
-			}));
-			}
-			
-			
+				$('#categories').show();
+			}	
 		} else {
 			console.log(this.checked);
 			$('#categories').hide();
