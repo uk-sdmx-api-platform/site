6c8f35057f3c36f2f32d7a456636d6fb83b042e0
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-16 15:46:17 +0000

    Deploying to gh-pages from  @ e4bb07299f2bad214d5c7123c9fabd8fee88bbc6 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3245,10 +3245,12 @@ function updateTimeSeriesAttributes(tsAttributeValues) {
 
 function initialiseFieldsWithGlobalValues(args) {
 
-  if (args.dataHasGlobalReportingType === False) {
+  if (args.dataHasGlobalReportingType === false) {
 		$('#toggles').hide()
 		return
   }
+	
+	let compareGlobalData = $('.toggle-switch-check').is(':checked');
 
   var template = _.template($('#categories_template').html());
   	$('#categories').html(template({
