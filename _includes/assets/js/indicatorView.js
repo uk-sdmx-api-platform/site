cf530b5f8f6de708eb3fb5e84516e9598d44e574
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-17 16:38:42 +0100

    Update indicatorView.js

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -210,6 +210,10 @@ var indicatorView = function (model, options) {
     $(OPTIONS.rootElement).on('change', '#serieses input', function () {
         MODEL.updateSelectedSeries($(this).val());
     });
+    
+    $(OPTIONS.rootElement).on('change', '#categories input', function () {
+        MODEL.updateSelectedComparisonValue($(this).val());
+    });
 
     $(OPTIONS.rootElement).on('click', '.variable-options button', function (e) {
         var type = $(this).data('type');
