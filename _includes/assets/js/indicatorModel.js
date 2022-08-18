bb65f54a8839c2100057af6c2a029bd969ca5596
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-18 17:58:14 +0100

    Update indicatorModel.js

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -136,6 +136,7 @@ var indicatorModel = function (options) {
 
   this.updateFieldStates = function(selectedFields) {
     this.selectedFields = helpers.removeOrphanSelections(selectedFields, this.edgesData);
+    console.log('selectedFields: '+this.selectedFields)
     this.allowedFields = helpers.getAllowedFieldsWithChildren(this.selectableFields, this.edgesData, selectedFields);
     this.fieldItemStates = helpers.getUpdatedFieldItemStates(this.fieldItemStates, this.edgesData, selectedFields, this.validParentsByChild);
     this.onSelectionUpdate.notify({
@@ -147,6 +148,7 @@ var indicatorModel = function (options) {
   this.updateSelectedFields = function (selectedFields) {
     this.updateFieldStates(selectedFields);
     this.getData();
+    console.log('selectedFields: '+this.selectedFields)
   };
   
   this.updateSelectedComparisonValue = function () {
