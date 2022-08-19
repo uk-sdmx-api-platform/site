319d5cb587d315ebe8b8e05f6adada40f4e746b2
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-19 11:44:41 +0100

    Update comparisonHelpers.js

--- _includes/assets/js/model/comparisonHelpers.js
+++ _includes/assets/js/model/comparisonHelpers.js
@@ -13,7 +13,7 @@ function updateSelectedFieldsFromSelectedValue(selectedComparisonValue) {
   if (value === "total") {
     // do nothing
   } else {
-    selectedFields.push(_.map($('#category-select'), function(option) {
+    selectedFields.push(_.map($('#category-select option'), function(option) {
       console.log(typeof selectedComparisonValue);
       console.log(selectedComparisonValue);
       return {
