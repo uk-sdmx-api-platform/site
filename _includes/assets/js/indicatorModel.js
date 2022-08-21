91f2c5585c506277f291d058ca5e1736e6e00524
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-21 19:36:12 +0100

    Update indicatorModel.js

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -83,7 +83,7 @@ var indicatorModel = function (options) {
       this.fieldsAreComparable = helpers.fieldsAreComparable(this.comparableFieldValues)
       this.headlineIsComparable = helpers.headlineIsComparable(this.headlineHasGlobalReportingType, this.headlineHasNationalReportingType)
       this.dataIsComparable = helpers.dataIsComparable(this.headlineIsComparable, this.fieldsAreComparable);
-      console.log('fieldColumns: ', getFieldColumnsFromData(this.allColumns))
+      console.log('fieldColumns: ', helpers.getFieldColumnsFromData(this.allColumns))
       console.log('allColumns: ', this.allColumns)
     }
   }
