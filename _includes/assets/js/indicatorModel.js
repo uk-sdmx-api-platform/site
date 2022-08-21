c397665e3b2fe66ffb9a52b0bf714fbf075c3a9f
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-21 14:03:27 +0100

    Update indicatorModel.js

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -158,13 +158,13 @@ var indicatorModel = function (options) {
   this.updateSelectedComparisonValue = function (selectedComparisonValue) {
     this.selectedFields = helpers.updateSelectedFieldsFromSelectedValue(selectedComparisonValue)
     this.getData();
-    console.log('selectedComparisonValue: '+selectedComparisonValue)
-    console.log('selectedFields: '+this.selectedFields)
+    console.log(selectedComparisonValue)
+    console.log('selectedFields: ',this.selectedFields)
   };
   
   this.updateHeadlineSelectedFields = function () {
     this.selectedFields = [{field: "Reporting type", values: ["National", "Global"]}];
-    console.log('selectedFields: '+this.selectedFields)
+    console.log('selectedFields: ',this.selectedFields)
     this.getData();
   }
     
