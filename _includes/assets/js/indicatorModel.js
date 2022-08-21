ce30b17e6e6b73a02a4899d07dc252f27e69f087
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-21 20:06:04 +0100

    Update indicatorModel.js

--- _includes/assets/js/indicatorModel.js
+++ _includes/assets/js/indicatorModel.js
@@ -72,14 +72,15 @@ var indicatorModel = function (options) {
   
   this.initialiseFieldsWithGlobalValues = function() {
     if (this.hasReportingTypes) {
+      var headline = helpers.getHeadline(this.selectableFields.filter(e => e != helpers.REPORTINGTYPE_COLUMN), this.data);
       this.fieldValuesWithGlobalReportingType = helpers.fieldValuesWithGlobalReportingType(this.data, this.allColumns);
       this.fieldValuesWithNationalReportingType = helpers.fieldValuesWithNationalReportingType(this.data, this.allColumns);
       this.comparableFieldValues = helpers.comparableFieldValues(this.data, this.allColumns)
       console.log('comparableFieldValues: ',this.comparableFieldValues)
       this.fieldItemStates = helpers.getInitialFieldItemStates(this.data, this.edgesData, this.allColumns, this.dataSchema);
       this.selectableFields = helpers.getFieldNames(this.fieldItemStates);
-      this.headlineHasGlobalReportingType = helpers.headlineHasGlobalReportingType(this.data);
-      this.headlineHasNationalReportingType = helpers.headlineHasNationalReportingType(this.data);
+      this.headlineHasGlobalReportingType = helpers.headlineHasGlobalReportingType(headline);
+      this.headlineHasNationalReportingType = helpers.headlineHasNationalReportingType(headline);
       this.fieldsAreComparable = helpers.fieldsAreComparable(this.comparableFieldValues)
       this.headlineIsComparable = helpers.headlineIsComparable(this.headlineHasGlobalReportingType, this.headlineHasNationalReportingType)
       this.dataIsComparable = helpers.dataIsComparable(this.headlineIsComparable, this.fieldsAreComparable);
