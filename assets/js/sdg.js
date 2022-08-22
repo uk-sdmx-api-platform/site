96aed5966512f69696865ec4f288d19140708e5e
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-22 12:59:18 +0000

    Deploying to gh-pages from  @ 75743b2fcb50158fa6a2e8d7fe35fd85dfa9301b 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3481,7 +3481,7 @@ function initialiseFieldsWithGlobalValues(args) {
 	$('.toggle-switch-check').change(function() {
 		if (this.checked) {
 			this.comparisonToggle = true;
-			console.log('comparisonToggle: ', comparisonToggle)
+			console.log('comparisonToggle: ', this.comparisonToggle)
 			if (args.headlineIsComparable) {
 				 MODEL.updateHeadlineSelectedFields()
 			}
@@ -3500,7 +3500,7 @@ function initialiseFieldsWithGlobalValues(args) {
 				console.log(args.comparableFieldValues)
 				$('#categories').show();
                                 $(OPTIONS.rootElement).on('change', '#category-select', function () {
-					console.log('comparisonToggle: ', comparisonToggle)
+					console.log('comparisonToggle: ', this.comparisonToggle)
 					console.log($(this).find(':selected').data('field'))
 					console.log($(this).val())
 					MODEL.updateSelectedComparisonValue($(this).find(':selected').data('field').concat("|",$(this).val()));
