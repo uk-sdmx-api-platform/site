1db01f6373010c4dc5a2cee1788a4833e28cb1bb
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-22 22:18:08 +0000

    Deploying to gh-pages from  @ a2d06fa3ebfc06ce388c7c8e00b8f9cc052c453e 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3008,7 +3008,14 @@ function fieldValueHasNationalReportingType(field, fieldValue, rows) {
   }
 
   // Before continuing, we may need to filter by Series, so set up all the Series stuff.
-  this.allData = helpers.prepareData(this.data);
+  
+   if (this.comparisonToggle) {
+      this.allData = helpers.prepareData(this.data);
+    }
+    else {
+      this.allData = helpers.prepareData(this.data).filter(key => (key['Reporting type'] != 'Global'))
+    }
+  
   console.log('allData: ', this.allData)
   this.allColumns = helpers.getColumnsFromData(this.allData);
   this.hasSerieses = helpers.dataHasSerieses(this.allColumns);
