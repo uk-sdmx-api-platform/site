72d0b91d47a0c98aeaae52ca13b41b400614f793
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-22 21:33:34 +0000

    Deploying to gh-pages from  @ b67561a5364f109bb1072349d9aee3ad05af235b 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3003,7 +3003,7 @@ function fieldValueHasNationalReportingType(field, fieldValue, rows) {
     this.fieldItemStates = helpers.getInitialFieldItemStates(this.data, this.edgesData, this.allColumns, this.dataSchema);
     this.validParentsByChild = helpers.validParentsByChild(this.edgesData, this.fieldItemStates, this.data);
     this.selectableFields = helpers.getFieldNames(this.fieldItemStates);
-    this.allowedFields = helpers.getInitialAllowedFields(this.selectableFields, this.edgesData);
+    this.allowedFields = helpers.getInitialAllowedFields(this.selectableFields, this.edgesData).filter(e => (e != 'Reporting type'));
     console.log('allowedFields: ',  this.allowedFields)
   }
 
