9d82a9e21f01dff204c5d176c7ed7470b785d0b8
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-23 01:57:36 +0100

    Update fieldHelpers.js

--- _includes/assets/js/view/fieldHelpers.js
+++ _includes/assets/js/view/fieldHelpers.js
@@ -8,7 +8,7 @@ function initialiseFields(args) {
     });
     if (fieldsContainValues) {
         var template = _.template($("#item_template").html());
-
+        console.log("args.allowedFields === ['Reporting type']: ",args.allowedFields === ['Reporting type'])
         if (!$('button#clear').length) {
             $('<button id="clear" disabled="disabled" aria-disabled="true" class="disabled">' + translations.indicator.clear_selections + ' <i class="fa fa-remove"></i></button>').insertBefore('#fields');
         }
