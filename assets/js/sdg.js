935430e54c331dec4afc83f5ed3c758459132d56
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-23 01:29:59 +0000

    Deploying to gh-pages from  @ d5ac34fe392ee0d6d24b65385193850dbc2f4842 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3365,13 +3365,14 @@ var HIDE_SINGLE_UNIT = true;
  * @return null
  */
 function initialiseFields(args) {
-    console.log('FIELDS INITIALISED!!!'
+    console.log('FIELDS INITIALISED!!!')
+    console.log('allowedFields: ', args.allowedFields)
+    console.log("args.allowedFields === ['Reporting type']: ",args.allowedFields === ['Reporting type'])
     var fieldsContainValues = args.fields.some(function (field) {
         return field.values.length > 0;
     });
     if (fieldsContainValues) {
         var template = _.template($("#item_template").html());
-        console.log("args.allowedFields === ['Reporting type']: ",args.allowedFields === ['Reporting type'])
         if (!$('button#clear').length) {
             $('<button id="clear" disabled="disabled" aria-disabled="true" class="disabled">' + translations.indicator.clear_selections + ' <i class="fa fa-remove"></i></button>').insertBefore('#fields');
         }
