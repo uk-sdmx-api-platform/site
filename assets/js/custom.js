8aca1f587e12748866e0f42e8b33d621451ca21e
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-08-30 12:39:39 +0000

    Deploying to gh-pages from  @ 5327385af30a94e937424380211bc7c7e171ab12 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,13 @@
 // Add any custom javascript here.
+
+opensdg.dataRounding = function(value) {
+  if (value == null) {
+    return value
+  }
+  else {
+    return Number(value.toPrecision(3))
+  }
+};
+
+$('td:contains("Not available for this indicator")')
+    .addClass('meta-not-available');
